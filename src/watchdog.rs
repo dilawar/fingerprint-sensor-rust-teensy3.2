77a6865c4e5e13d0786817abbae05814cbf5590e
Dilawar Singh <dilawar.s.rajput@gmail.com>
2026-01-04 13:24:17 +0530

    Use old create for volatile and bit_field

--- src/watchdog.rs
+++ src/watchdog.rs
@@ -1,37 +1,40 @@
+use bit_field::BitField;
 use core::arch::arm::__nop;
+use volatile::Volatile;
 
 #[repr(C)]
 pub struct Watchdog {
-    stctrlh: u16,
-    stctrll: u16,
-    tovalh: u16,
-    tovall: u16,
-    winh: u16,
-    winl: u16,
-    refresh: u16,
-    unlock: u16,
-    tmrouth: u16,
-    tmroutl: u16,
-    rstcnt: u16,
-    presc: u16,
+    stctrlh: Volatile<u16>,
+    stctrll: Volatile<u16>,
+    tovalh: Volatile<u16>,
+    tovall: Volatile<u16>,
+    winh: Volatile<u16>,
+    winl: Volatile<u16>,
+    refresh: Volatile<u16>,
+    unlock: Volatile<u16>,
+    tmrouth: Volatile<u16>,
+    tmroutl: Volatile<u16>,
+    rstcnt: Volatile<u16>,
+    presc: Volatile<u16>,
 }
 
 impl Watchdog {
     pub unsafe fn new() -> &'static mut Watchdog {
-        &mut *(0x4005_2000 as *mut Watchdog)
+        unsafe { &mut *(0x4005_2000 as *mut Watchdog) }
     }
 
     pub fn disable(&mut self) {
         unsafe {
             // unlock the watchdog registers first before we can disable it.
-            core::ptr::write_volatile(&mut self.unlock, 0xC520);
-            core::ptr::write_volatile(&mut self.unlock, 0xD928);
+            self.unlock.write(0xC520);
+            self.unlock.write(0xD928);
             __nop();
             __nop();
+
             // now disable.
-            let mut ctrl = core::ptr::read_volatile(&self.stctrlh);
-            ctrl &= !(0x0000_0001);
-            core::ptr::write_volatile(&mut self.stctrlh, ctrl);
+            self.stctrlh.update(|ctrl| {
+                ctrl.set_bit(0, false);
+            });
         }
     }
 }
